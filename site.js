a8e14eac2b907ecba4f135b55d75e344816a155c
sjaworowicz <125sjaworowicz@frhsd.com>
2025-02-27 08:35:24 -0500

    step 7 slide 15

--- site.js
+++ site.js
@@ -41,8 +41,50 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            getMonthText: function (dateArray) {
-                  alert(dateArray)
+            getMonthText(dateArray) {
+              console.log(dateArray)
+                  let month;
+            switch(dateArray[1]) {
+                  case 1:
+                  month = "January";
+                        break;
+                      case 2:
+                        month = "February";
+                        break;
+                      case 3:
+                        month = "March";
+                        break;
+                      case 4:
+                        month = "April";
+                        break;
+                      case 5:
+                        month = "May";
+                        break;
+                      case 6:
+                        month = "June";
+                        break;
+                      case 7:
+                        month = "July";
+                        case 8:
+                        month = "August";
+                        break;
+                      case 9:
+                        month = "September";
+                        break;
+                      case 10:
+                        month = "October";
+                        break;
+                      case 11:
+                        month = "November";
+                        break;
+                      case 12:
+                        month = "December";
+            };
+            return month + " " + dateArray[2] + ", " + dateArray[0];
+            },
+            likeIncrement(count) {
+              count++
+              console.log();
             }
       }
 })
